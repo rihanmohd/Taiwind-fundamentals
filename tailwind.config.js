902164b24cdbe407e93236c27860e006a5f508dd
rihanmohd <forlearningrihan@gmail.com>
2024-08-12 22:41:27 +0530

    Loopstudios web tailcss
    web prj by tailcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,19 @@
-
 module.exports = {
   content: ['./*.html'],
   theme: {
-    screens: {
-       sm: '480px',
-       md: '768px',
-       lg: '976px',
-       xl: '1440px'
+    screnns: {
+      sm: '480px',
+      md: '768px',
+      lg: '976',
+      xl: '1440px',
     },
     extend: {
-      colors: {
-        strongCyan: 'hsl(171, 66%, 44%)',
-        lightBlue: 'hsl(233, 100%, 69%)',
-        darkGrayishBlue: 'hsl(210, 10%, 33%)',
-        grayishBlue: 'hsl(201, 11%, 66%)',
-      },
       fontFamily: {
-        sans: ['Bai jamjuree', 'sans-serif'],
-
+        sans: ['josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.3em',
       }
     },
   },
