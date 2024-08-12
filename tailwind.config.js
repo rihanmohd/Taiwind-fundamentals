6127934649ec08e242d38ddece875643e57778ac
rihanmohd <forlearningrihan@gmail.com>
2024-08-12 22:46:22 +0530

    shortly web tailcss
    web prj by tailcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    screnns: {
-      sm: '480px',
-      md: '768px',
-      lg: '976',
-      xl: '1440px',
-    },
     extend: {
-      fontFamily: {
-        sans: ['josefin Sans', 'sans-serif'],
-        alata: ['Alata'],
+      colors: {
+        cyan: 'hsl(180, 66%, 49%)',
+        cyanLight: 'hsl(180, 66%, 69%)',
+        darkViolet: 'hsl(257, 27%, 26%)',
+        red: 'hsl(0, 87%, 67%)',
+        grayishViolet: 'hsl(257, 7%, 63%)',
+        veryDarkBlue: 'hsl(255, 11%, 22%)',
+        veryDarkViolet: 'hsl(260, 8%, 14%) ',
       },
-      letterSpacing: {
-        widest: '.3em',
-      }
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      spacing: {
+        180: '32rem',
+      },
     },
   },
   plugins: [],
-}
-
+} 
