da1fad59dc0ff3724d9660ebf6dcdcf7c9dfb7bc
rihanmohd <forlearningrihan@gmail.com>
2024-08-15 22:58:40 +0530

    Bookmark manager web tailcss
    web prj by tailcss

--- js/script.js
+++ js/script.js
@@ -1,49 +1,46 @@
-const themeToggleBtn = document.getElementById('theme-toggle')
-const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon')
-const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon')
+const tabs = document.querySelectorAll('.tab')
+const panels = document.querySelectorAll('.panel')
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+const logo = document.getElementById('logo')
+
+// Tabs menu event listener
+tabs.forEach((tab) => tab.addEventListener('click', onTabClicke))
+
+// Hamburger btn listener
+btn.addEventListener('click', navToggle)
 
 
-if (
-    localStorage.getItem('color-theme') === 'dark' ||
-    (!('color-theme' in localStorage) &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches)
-  ) {
-    // show light icon
-    themeToggleLightIcon.classList.remove('hidden')
-  } else {
-    themeToggleDarkIcon.classList.remove('hidden')
+function onTabClicke (e) {
+    // deactivate all tabs
+tabs.forEach((tab) => {
+    tab.children[0].classList.remove(
+        'border-softRed',
+           'border-b-4',
+        'md:border-b-0')
+})
+
+// Hide all panels
+panels.forEach((panel) => panel.classList.add('hidden'))
+
+// Activate a new tab panel bassed on the target
+e.target.classList.add('border-softRed', 'border-b-4')
+
+const classString = e.target.getAttribute('data-target')
+document.getElementById('panels')
+.getElementsByClassName(classString)[0]
+.classList.remove('hidden')
+}
+
+function navToggle() {
+    btn.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+
    
-  }
-
-//   listen for toggle button click
-themeToggleBtn.addEventListener('click', toggleMode)
-
-function toggleMode() {
-    // toggle icon
-    themeToggleDarkIcon.classList.toggle('hidden')
-    themeToggleLightIcon.classList.toggle('hidden')
-
-
-
-
-    // if is set in loclsrg 
-    if(localStorage.getItem('color-theme')) {
-    //  if light. make dark and save in loclsrg
-    if(localStorage.getItem('color-theme') === 'light') {
-        document.documentElement.classList.add('dark')
-        localStorage.setItem('color-theme', 'dark')
+    if(menu.classList.contains('flex')){
+        logo.setAttribute('src', './images/logo-bookmark-footer.svg')
     } else {
-        document.documentElement.classList.remove('dark')
-        localStorage.setItem('color-theme', 'light')
-    }
-    } else {
-        // if not in locltstrg
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark')
-            localStorage.setItem('color-theme', 'light')
-        } else {
-            document.documentElement.classList.add('dark')
-            localStorage.setItem('color-theme', 'dark')
-        }
+        logo.setAttribute('src', './images/logo-bookmark.svg')
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,25 @@
 module.exports = {
   content: ['./*.html'],
-  darkMode: 'class',
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
     extend: {
       colors: {
-        darkBlue: 'hsl(217, 28%, 15%)',
-        darkBlue1: 'hsl(218, 28%, 13%)',
-        darkBlue2: 'hsl(216, 53%, 9%)',
-        darkBlue3: 'hsl(219, 30%, 18%)',
-        accentCyan: 'hsl(176, 68%, 64%)',
-        accentBlue: 'hsl(198, 60%, 50%)',
-        lightRed: 'hsl(0, 100%, 63%)',
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
       },
       fontFamily: {
-        sans: ['Raleway', 'sans-serif'],
-        opensans: ['Open Sans', 'sans-serif'],
+        sans: ['Rubik', 'sans-serif'],
       },
-      backgroundImage: (theme) => ({
-        'logo-dark-mode': "url('../images/logo-dark-mode.svg')",
-        'logo-light-mode': "url('../images/logo-light-mode.svg')",
-        'curvy-dark-mode': "url('../images/bg-curvy-dark-mode.svg')",
-        'curvy-light-mode': "url('../images/bg-curvy-light-mode(3).svg')",
+      backgroundImage: () => ({
+        dots: "url('../images/bg-dots.svg')",
       }),
-    },
-  },
-  variants: {
-    extend: {
-      backgroundImage: ['dark'],
     },
   },
   plugins: [],
